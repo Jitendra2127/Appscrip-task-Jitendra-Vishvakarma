5e096faadc0bc10652aa814c14b0b745337be5af
unknown <jitendravishvakramjitu2a@gmail.com>
2024-04-14 12:09:34 +0530

    Second

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,7 @@ const page = async() => {
   const data=await dataFetch();
   return (
     <main className="">
+      <h1 className="text-5xl">jitendra</h1>
       
         
        <Navbar navbar_data={navbar_data}/>
